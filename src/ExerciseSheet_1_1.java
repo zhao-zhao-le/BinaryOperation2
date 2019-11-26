6012eae1a4a6c51c455bc50029918f9e23d49616
zhao-zhao-le <57627260+zhao-zhao-le@users.noreply.github.com>
2019-11-26 15:52:16 +0800

    Revert "版本1.0"
    This reverts commit 013d316d7b7ff48f4ed92272bb553260bb5b968a.

--- src/ExerciseSheet_1_1.java
+++
@@ -1,9 +0,0 @@
-
-public class ExerciseSheet_1_1 {
-	public static void main(String[] args) {
-		Exercise_1_1 exercise = new Exercise_1_1();
-		exercise.generateBinaryExercisee(100);
-		System.out.println("----------generate and display add exercise----------");
-		exercise.formattedDisplay(5);
-	}
-}
